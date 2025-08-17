efb63364020001bea8a70cc6c230bca849c10105
prashantk01 <kumarprince2510@gmail.com>
2025-08-17 15:19:52 +0530

    project setup: fit-check

---
+++ src/main/java/com/fitcheck/fit_check/FitCheckApplication.java
@@ -0,0 +1,13 @@
+package com.fitcheck.fit_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitCheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitcheck/fit_check/FitCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitcheck.fit_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitCheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
